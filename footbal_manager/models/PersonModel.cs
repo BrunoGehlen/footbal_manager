7e901057a4be4695dbc1ed00b7e4274e37558ff5
Bruno Gehlen <tousepc@gmail.com>
2020-06-21 14:01:44 -0300

    Add text file ModelNotes

---
+++ footbal_manager/models/PersonModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace footbal_manager {
+
+    public class PersonModel {
+        //Variables
+        //Strings
+        public string name;
+        public string nationality;
+        //ints
+        public int age;
+        public int moral;
+        public int ability;
+
+        // Person's constructor
+        public PersonModel(string name, string nationality, int age, int moral, int ability) {
+
+            this.name = name;
+            this.nationality = nationality;
+            this.age = age;
+            this.moral = moral;
+            this.ability = ability;
+
+            }
+        }
+    }
